10bcd1ce9f6b3e23993e9dae20bb66122656628e
Casey Snepar <sneparc.gmail.com>
2019-03-03 16:11:39 -0500

    first commit

---
+++ apps/app.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+
+get '/home' do 
+	erb :home
+end
+	
+get '/teachingCareer' do
+	erb :teachingCareer
+end
+
+get '/portfolio' do
+	erb :portfolio
+end
+
+get '/JC' do
+	erb :JC
+end
+
+get '/RomulusandRemus' do
+	erb :RomulusandRemus
+end
+
+get '/Neptune' do
+	erb :Neptune
+end
+
+get '/Colosseum' do
+	erb :Colosseum
+end
+
+get '/Basic_Two_Pager_Casey_Snepar_Page_1' do
+	erb :Basic_Two_Pager_Casey_Snepar_Page_1
+end
+
+get '/Basic_Two_Pager_Casey_Snepar_Page_2' do
+	erb :Basic_Two_Pager_Casey_Snepar_Page_2
+end
+
+get '/CSS_Integration' do
+	erb :CSS_Integration
+end
+
+get '/NYCDA_Times' do
+	erb :NYCDA_Times
+end
+
+get '/example_Article' do
+	erb :example_Article
+end
+
+get '/DIV_layout' do
+	erb :DIV_layout
+end
+
+get '/the_onion' do
+	erb :the_onion
+end
+
+get '/responsive_web' do
+	erb :responsive_web
+end
